1b618fdeefa3801e5a81208a0f48cde5b293ec62
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2025-12-08 15:26:26 +0200

    folder and dependecies init

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Pro Store",
+  description: "Pro Store is a platform for buying and selling products",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
