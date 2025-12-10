c23fbc2b5578769cd8c16b0b92179af5e31889ee
Ahmed Alaa <92605303+AhmedAlaa21@users.noreply.github.com>
2025-12-11 00:42:05 +0200

    prisma setup

--- db/seed.ts
+++ db/seed.ts
@@ -1,13 +1,22 @@
-import { PrismaClient } from "@/lib/generated/prisma/client";
-import sampleData from "./sample-data";
+import "dotenv/config";
+import sampleData from "@/db/sample-data";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 async function main() {
-  const prisma = new PrismaClient();
-  await prisma.product.deleteMany();
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  const prisma = new PrismaClient({ adapter });
 
-  await prisma.product.createMany({ data: sampleData.products });
-
-  console.log("Database seeded successfully!");
+  try {
+    await prisma.product.deleteMany();
+    await prisma.product.createMany({ data: sampleData.products });
+    console.log("Database seeded successfully!");
+  } finally {
+    await prisma.$disconnect();
+    await pool.end();
+  }
 }
 
 main();
