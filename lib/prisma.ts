6100a7b1d301c6defa3bbf8f8a7ae342ff98c7b9
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2025-12-11 19:34:57 +0200

    Update Prisma configuration to include WebSocket support for Neon database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
 
+neonConfig.webSocketConstructor = ws;
 const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaNeon({
-  connectionString,
-});
+
+const adapter = new PrismaNeon({ connectionString });
 
 const prisma = new PrismaClient({
   adapter,
