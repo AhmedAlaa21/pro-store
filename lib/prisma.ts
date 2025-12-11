aa62f285fff9b29970f27d78f763ebf4db8b3cf2
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2025-12-11 19:25:04 +0200

    done with serverless actions and neon with prisma configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,28 @@
 import { PrismaClient } from "../app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaNeon({
+  connectionString,
 });
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient({
+  adapter,
+}).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
 
 export default prisma;
